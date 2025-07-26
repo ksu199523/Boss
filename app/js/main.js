40d17a4224ce17a2853849736a9945ef77827414
ksu199523 <ksergeevna1407@gmail.com>
2025-07-26 21:59:50 +0300

    added page product

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.lookperfect__slider', {
+
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 40,
+  
+  navigation: {
+    nextEl: '.lookperfect__arrow-next',
+    prevEl: '.lookperfect__arrow-prev',
+  },
+
+});   
